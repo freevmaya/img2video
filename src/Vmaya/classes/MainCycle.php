3a3388d05b68ca2788d4e4bd653a785ea74826a9
Vadim Frolov <@vmaya>
2025-12-14 15:56:30 +0500

    requlary

--- src/Vmaya/classes/MainCycle.php
+++ src/Vmaya/classes/MainCycle.php
@@ -210,7 +210,7 @@ class MainCycle {
 
     protected function scraperDownload($url, $file_path) {
         $output = null;
-        $command = 'py '.BASEPATH."scraper_download.py \"{$url}\" \"{$file_path}\"";
+        $command = 'python '.BASEPATH."scraper_download.py \"{$url}\" \"{$file_path}\"";
 
         exec($command, $output);
         
