d32dd532d48da703e19e0a3e8f101b81a06ae925
Vadim Frolov <@vmaya>
2025-12-14 17:17:20 +0500

    requlary

--- download_test.php
+++ download_test.php
@@ -1,5 +1,5 @@
 <?
-	$url = "https://cdn.midjourney.com/465acea3-64cc-45ab-81dd-501533b489f5/0_0.png";
+	$url = "https://cdn.midjourney.com/video/043f5f8c-913e-45a5-a87e-9efc8bc866ef/2.mp4";
 	$file_path = "/home/vmaya/www/img2video/downloads/1.png";
 	$output = null;
     $command = "python3 scraper_download.py \"{$url}\" \"{$file_path}\"";
