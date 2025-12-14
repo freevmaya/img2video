dee3cc17d9e22163fe0e6d36c5d57d7287749c8d
Vadim Frolov <@vmaya>
2025-12-14 17:14:46 +0500

    requlary

--- scraper_download.py
+++ scraper_download.py
@@ -11,6 +11,8 @@ if len(sys.argv) > 2:
     #print('Status Code:', response.status_code)
     #print('File size (bytes):', len(response.content))
 
+    print(sys.argv)
+
     if response.status_code == 200:
         with open(sys.argv[2], 'wb') as f:
             f.write(response.content)
