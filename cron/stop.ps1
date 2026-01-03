4b160aa54ff342fc2d8b92bca718ecf896c56e7e
Vadim Frolov <@vmaya>
2026-01-03 13:33:08 +0500

    requlary

--- cron/stop.ps1
+++
@@ -1,8 +0,0 @@
-if (Test-Path "pids.txt") {
-    $pids = Get-Content "pids.txt"
-    foreach ($apid in $pids) {
-        Stop-Process -Id $apid -Force -ErrorAction SilentlyContinue
-        Write-Host "Stop process: $apid"
-    }
-    Remove-Item "pids.txt"
-}
