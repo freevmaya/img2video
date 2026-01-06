2636bbd6c640452645a9ad42f5e718be5785d794
Vadim Frolov <@vmaya>
2026-01-06 15:51:34 +0500

    requlary

--- src/Vmaya/services/cycle/LeoCycle.php
+++ src/Vmaya/services/cycle/LeoCycle.php
@@ -11,16 +11,70 @@ class LeoCycle extends BaseCycle {
 
         if (method_exists($this, $method)) {
             if ($response['status'] == 'COMPLETE') {
-                $this->$method($task, $response);
+                if ($this->$method($task, $response)) 
+                    $this->finishResponse($response); 
             }
-
-            $this->finishResponse($response); 
         }
         else {
             $this->finishResponse($response);
             trace_error("The method is missing: {$method}");
         }
 	}
+
+    protected function process_video_generation($task, $response) {
+        $result = json_decode($response['data'], true);
+        if (!isset($result['object']['images'][0]['motionMP4URL'])) {
+            trace_error("Unknown result: ".$response['data']);
+            return false;
+        }
+
+        $file_url = $result['object']['images'][0]['motionMP4URL'];
+        $info = pathinfo(explode('?', $file_url)[0]);
+        $file_name = $task['hash'].'-'.$response['id'].'.'.$info['extension'];
+
+        $method_data = [
+            'this'      => $this,
+            'task'      => $task,
+            'response'  => $response,
+            'file_url'  => $file_url,
+            'file_name' => $file_name,
+            'file_path' => RESULT_PATH.$file_name
+        ];
+
+        $this->parent->downloadClient->AddTask(function($record, $data) {
+            $method     = 'select';
+            $task       = $data['task'];
+            $response   = $data['response'];
+            $hash       = $task['hash'];
+            $dl_state   = $record['state'];
+            $_this      = $data['this'];
+            $parent     = $_this->parent;
+
+            trace("Attempt send photo {$data['file_path']}");
+
+            resizeImageIfTooLarge($data['file_path']);
+
+            if ($dl_state == 'finished') {
+                $result = $parent->sendMp4($task, $data['file_path'], $data['file_name'], Lang('Your photo is ready'));
+
+                if ($result) {
+
+                    $parent->PayUpscale($task['user_id'], [
+                        'response_id'=>$response['id'],
+                        'hash'=>$hash
+                    ]);
+
+                    $parent->finishTask($task, 'finished');
+                } else {
+                    $parent->Message($task['chat_id'], Lang('Something wrong'));
+                    $parent->finishTask($task, 'failure');
+                }
+            } else {
+                $parent->Message($task['chat_id'], Lang('Fail download image'));
+                $parent->finishTask($task, 'failure');
+            }
+        }, $method_data['file_url'], $method_data['file_path'], $method_data);
+    }
 
     protected function process_image_generation($task, $response) {
 
