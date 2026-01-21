d31ca6b71997e0462a369675b89d8a6ca556e32c
Vadim Frolov <@vmaya>
2026-01-21 17:48:38 +0500

    requlary

--- src/Vmaya/classes/BaseBot.php
+++ src/Vmaya/classes/BaseBot.php
@@ -509,7 +509,7 @@ abstract class BaseBot extends SettingsManager {
         
         $date = $chatMember['date'];
 
-        $this->SendToOwnerChangeStatus($chat, $from, $oldStatus, $newStatus);
+        //$this->SendToOwnerChangeStatus($chat, $from, $oldStatus, $newStatus);
 
         return true;
     }
